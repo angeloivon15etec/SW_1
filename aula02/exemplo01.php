b4601d174d2c3d43c34da7de853103f6cf803070
angeloivon15etec <161508578+angeloivon15etec@users.noreply.github.com>
2025-02-14 16:54:33 -0300

    Add files via upload

---
+++ aula02/exemplo01.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo 01 - PHP</title>
+</head>
+<body>
+    <p>Oi sou o Angelo</p>
+    
+    <?php
+     echo "<p>Oi sou o Angelo</p>";
+    
+     $nome = "Angelo";
+     echo"<p> Oi sou o $nome </p>";
+     echo"<p> Oi sou o ".$nome."</p>";
+
+
+     ?>
+
+
+
+</body>
+</html>
