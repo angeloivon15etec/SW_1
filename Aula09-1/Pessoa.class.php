9e6c75cadf36bb153dc1a792fcfda1a954024bdb
angeloivon15etec <161508578+angeloivon15etec@users.noreply.github.com>
2025-05-09 15:13:56 -0300

    Add files via upload

---
+++ Aula09-1/Pessoa.class.php
@@ -0,0 +1,19 @@
+<?php
+    class Pessoa{
+        public $Nome;
+        public $Peso;
+        public $Altura;
+    
+    public function MostrarDados(){
+        echo "Nome da pessoa". $this ->Nome. "<br>";
+        echo "Peso da pessoa". $this ->Peso. "<br>";
+        echo "Altura da pessoa". $this ->Altura. "<br>";
+        echo "<hr>";
+        
+    }
+
+
+
+    }
+
+?>
